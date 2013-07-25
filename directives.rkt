55b16a41d630c1e25df9624ac6734e00aa1d11a4
Ryan Plessner <rpless@ccs.neu.edu>
2013-07-25 19:44:25 -0400

    added the hold-position directive and tests for it

--- directives.rkt
+++ directives.rkt
@@ -27,7 +27,8 @@
 ;; 2Vector -> 2Vector
 (define (handle-directive directive positionable)
   (match directive
-    [(move-toward pos) (handle-move-directive pos positionable)]))
+    [(move-toward pos) (handle-move-directive pos positionable)]
+    [(hold-position) (send positionable position)]))
 
 ;; 2Vector -> 2Vector
 (define (handle-move-directive goal positionable)
@@ -35,3 +36,25 @@
     (if (<= (distance pos goal) SPEED)
         goal
         (+ pos (* SPEED (normalize (- goal pos)))))))
+
+;; Tests
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(module+ test
+  (require rackunit)
+  (define test-positionable% (class object%
+                               (init pos) 
+                               (define in:position pos) 
+                               (define/public (position) in:position)
+                               (super-new)))
+  
+  (let ()
+    ;; Example
+    (define s1 (make-object test-positionable% 0+0i))
+    
+    ;; Move Directives
+    (check-equal? (handle-directive (move-toward 10+0i) s1) (make-rectangular SPEED 0))
+    (check-equal? (handle-directive (move-toward 0+0i) s1) 0+0i)
+    
+    ;; Hold Directive
+    (check-equal? (handle-directive (hold-position) s1) (send s1 position))))
